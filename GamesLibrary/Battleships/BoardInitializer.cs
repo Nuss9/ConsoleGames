5556ea19c89c15c819709e3c70d9715346756edc
Dennis Jakupovic <djakupovic@winbase.nl>
2019-09-10 12:17:10 +0200

    Reset initial Battleship tests & implementation

--- GamesLibrary/Battleships/BoardInitializer.cs
+++ GamesLibrary/Battleships/BoardInitializer.cs
@@ -1,39 +1,7 @@
-using System;
-using System.Collections.Generic;
-
 namespace GamesLibrary.Battleships
 {
 	public class BoardInitializer
 	{
-		public int BoardWidth { get; set; }
-		public int BoardLength { get; set; }
-		public List<Ship> Fleet { get; set; }
 
-		public BoardInitializer()
-		{
-			this.BoardLength = 5;
-			this.BoardWidth = 5;
-			this.Fleet = new List<Ship>();
-		}
-
-		public void PlaceSubmarine()
-		{
-			var submarineLocation = GetRandomPoint();
-			Submarine boat = new Submarine();
-			boat.Coordinates.Add(submarineLocation);
-			this.Fleet.Add(boat);
-		}
-
-		private Point GetRandomPoint()
-		{
-			Random random = new Random();
-			int xCoordinate = random.Next(1, this.BoardWidth);
-			int yCoordinate = random.Next(1, this.BoardLength);
-
-			return  new Point {
-				XCoordinate = xCoordinate,
-				YCoordinate = yCoordinate
-			};
-		}
 	}
 }
--- GamesLibraryTests/BattleshipsTests/BoardInitializerTests.cs
+++ GamesLibraryTests/BattleshipsTests/BoardInitializerTests.cs
@@ -13,22 +13,9 @@ namespace GamesLibraryTests.BattleShipTests
 		}
 
 		[Fact]
-		public void WhenInitializingGame_ItShouldSetDefaultBoundaries()
+		public void When_ItShould()
 		{
-			int expected = 5;
-
-			Assert.Equal(expected, subject.BoardWidth);
-			Assert.Equal(expected, subject.BoardLength);
-		}
-
-		[Fact]
-		public void WhenASubmarineIsPlaced_ItShouldOccupyOneCoordinateOnThePlayingboard()
-		{
-			subject.PlaceSubmarine();
-
-			Assert.Single<Point>(subject.Fleet[0].Coordinates);
-			Assert.InRange(subject.Fleet[0].Coordinates[0].XCoordinate, 1, subject.BoardWidth);
-			Assert.InRange(subject.Fleet[0].Coordinates[0].YCoordinate, 1, subject.BoardLength);
+			
 		}
 	}
 }
